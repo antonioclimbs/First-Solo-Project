5da566178580d61105912f694539880f89474a11
Antonio Ayala <antonioayala@Antonios-MBP.attlocal.net>
2022-05-06 09:16:55 -0700

    minor updates

--- client/App.jsx
+++ client/App.jsx
@@ -1,20 +1,59 @@
 import React, { component } from 'react'
 import { render } from 'react-dom'
 // import MainContainer from './containers/MainContainer'
-import Button from './components/Button'
+// import Button from './components/Button'
 
 console.log('getting to app')
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      points: 0,
+      upgrades: [],
+      multiplier: 1
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    let result = this.state.points;
+    multiplier = this.state.multiplier;
+    result = result + multiplier;
+    this.setState((state) => {
+      return (
+        {
+          points: result
+        }
+      )
+    })
+
+  }
 
   render() {
     return (
-      <div>
-        <h1>App Header</h1>
+      <div className='container'>
         <Button />
+      </div>
+    )
+  }
+}
+
+class Button extends React.Component {
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => { this.props.handleClick(); }}
+
+        // onClick={(e) => {
+        //   e.preventDefault;
+        //   console.log('getting to button', e);
+        //   props.addPoints();
+        //   console.log(props.points);
+        // }}
+        >Throw an Error!</button>
+        <h6>It's a Feature, Not a Bug</h6>
       </div>
     )
   }
